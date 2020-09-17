6e9aec5579590006a262f08a2e5e727c5228bc10
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-17 07:03:50 -0400

    debug powerup console

--- src/main/java/io/github/sbcloudrace/sbtranslator/debug/DebugController.java
+++ src/main/java/io/github/sbcloudrace/sbtranslator/debug/DebugController.java
@@ -5,11 +5,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -26,7 +30,14 @@ public class DebugController {
     public @ResponseBody
     String allRequests(HttpServletRequest request, HttpServletResponse response) {
         response.setHeader("Connection", "close");
-        System.out.println(request.getMethod() + " -> " + request.getRequestURL());
+
+        String categoryName = request.getParameter("categoryName");
+        if (categoryName != null && "STORE_POWERUPS".equals(categoryName)) {
+            System.out.println(categoryName);
+            return readFile(request.getRequestURI().substring(1).concat("_").concat(categoryName).concat(".xml"));
+        }
+
+        System.out.println(request.getMethod() + " -> " + request.getRequestURL() + " ? " + request.getQueryString());
 
         return readFile(request.getRequestURI().substring(1).concat(".xml"));
     }
