a47889c1efa3a2804cf426cb245c73cb0da729ea
Nilzao <288571+nilzao@users.noreply.github.com>
2020-09-17 21:43:52 -0400

    sb-persona feign client params

--- src/main/java/io/github/sbcloudrace/sbtranslator/sbpersona/SbPersonaServiceProxy.java
+++ src/main/java/io/github/sbcloudrace/sbtranslator/sbpersona/SbPersonaServiceProxy.java
@@ -1,10 +1,14 @@
 package io.github.sbcloudrace.sbtranslator.sbpersona;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "sb-persona", url = "localhost:8501", path = "/sb-persona")
 public interface SbPersonaServiceProxy {
 
     //http://localhost:8501/sb-persona/sbpersonas/100
-   SbPersona getPersonaById();
+    @RequestMapping(value = "/sbpersonas/{personaId}", method = RequestMethod.GET)
+    SbPersona getPersonaById(@PathVariable String personaId);
 }
